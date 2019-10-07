66f19b73993757af9f4a7dea9f8cd61ed04870d5
robertnajiblansangan <55092070+robertnajiblansangan@users.noreply.github.com>
2019-10-07 20:27:32 +0800

    Refactored and Passed the NPM Test

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
+var total = 0;
+
 module.exports = function main(inputs) {
-//    console.log("Debug Info");
-//    var expected = printReceipt(inputs)
-//    return expected;
-
-        var id = [];
-        for (var x in inputs){
-    //       data[y].push({Barcode: inputs[y].barcode, Name: inputs[y].Name, Unit: inputs[y].Unit, Price:inputs[y].Price});]
-           var count = 0;
-              for (var y in id){
-                if (inputs[x].Barcode === id[y].Barcode){
-                count++;
-                }
-              }
-
-             if (count > 0 ){
-                console.log('***<store earning no money>Receipt ***\n');
-                var barcode = inputs[x].Barcode
-                id.push('item001');
-           }
-        }
-
-        return id;
+    console.log("Debug Info");
+    return printReceipt(inputs);
  };
 
-//function printReceipt(inputs){
-//
-////    var expected text = '***<store earning no money>Receipt ***\n';
-//    var id = [];
-//    for (var x in inputs){
-////       data[y].push({Barcode: inputs[y].barcode, Name: inputs[y].Name, Unit: inputs[y].Unit, Price:inputs[y].Price});
-//         if (id.find(inputs[x].Barcode) != ''){
-//            console.log('***<store earning no money>Receipt ***\n');
-//            id.push(inputs[x].Barcode);
-//       }
-//    }
-//
-//    return inputs;
-//}
+
+
+function printReceipt(inputs){
+    let products = inputs;
+    var receipt = '***<store earning no money>Receipt ***\n';
+    var barcodes = distinctBarcode(products);
+
+    barcodes.forEach(barcode => {
+        receipt = receipt + getBodyOfReceipt(barcode, inputs);
+    });
+
+    receipt = receipt + '----------------------\n' +
+                        'Total: '+total.toFixed(2)+' (yuan)\n' +
+                        '**********************\n';
+
+    return receipt;
+}
+
+function distinctBarcode (products){
+    var barcodesCollection = []; 
+
+    products.forEach(product => {
+        barcodesCollection.push(product.Barcode);
+    });
+
+    return [... new Set(barcodesCollection)]; 
+}
+
+function getBodyOfReceipt(barcode, inputs){
+    var name = 0;
+    var quantity = 0;
+    var price = 0; 
+    var unit;
+    var subtotal = 0;
+
+    inputs.forEach(product => {
+        if (product.Barcode === barcode){
+            quantity++; 
+            name = product.Name;
+            unit = product.Unit; 
+            price = parseInt(product.Price);
+        }
+    });
+
+    if (unit === 'a') {
+        unit = ''
+    }else
+    {
+        unit = ' '+unit+'s';
+    };
+
+    subtotal = price * quantity; 
+    total += subtotal;
+    
+    return 'Name: ' + name + 
+    ', Quantity: ' + quantity + unit + 
+    ', Unit price: '+ price.toFixed(2) + 
+    ' (yuan), Subtotal: '+ subtotal.toFixed(2) + ' (yuan)\n'; 
+
+}
